b902435bf95329674054e41527683d1137e35331
Anmol Muskan <98347170+Muske2@users.noreply.github.com>
2023-09-05 16:02:17 +0530

    Update Q2.py

--- Lab 4/Q2.py
+++ Lab 4/Q2.py
@@ -2,9 +2,9 @@ import cmath
 
 x=int(input("Enter real value of complex number: "))
 y=int(input("Enter imaginary value of complex number: "))
-z=complex(i,j)
+z=complex(x,y)
 
 print("Complex number= "+str(z.real)+" + "+str(z.imag)+"j")
 print("Square root= "+str(cmath.sqrt(z)))
-print("Log value= "+str(cmath.log2(z)))
+print("Log value= "+str(cmath.log(z)))
 print("Sine value= "+str(cmath.sin(z)))
